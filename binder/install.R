bc9295aa7a67eb63a6db95ecdf599ae3a9e44ff7
monaghaa <33913248+monaghaa@users.noreply.github.com>
2021-08-02 02:49:42 -0600

    Add files via upload

---
+++ binder/install.R
@@ -0,0 +1,18 @@
+# Data analysis
+install.packages("tidyverse")
+# R Markdown documents converter
+install.packages("rmarkdown")
+# HTTP tools
+install.packages("httr")
+# Shiny
+install.packages("shinydashboard")
+# Interactive maps
+install.packages('leaflet')
+#others
+install.packages('ggstatsplot')
+install.packages('psych')
+install.packages('knitr')
+install.packages('broom')
+install.packages('fivethirtyeight')
+install.packages('ggpubr')
+
